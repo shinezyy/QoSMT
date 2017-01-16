600c5fc1a715ee3414f32783e089d8d7652d5f92
Zhou Yaoyang <diamondzyy@sina.com>
2017-01-16 14:20:37 +0800

    adding run scripts

---
+++ smt_run/perl.sh
@@ -0,0 +1,8 @@
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;perlbench" -o ~/dynamic/perl_perl -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;bzip2" -o ~/dynamic/perl_bzip2 -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;gcc" -o ~/dynamic/perl_gcc -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;mcf" -o ~/dynamic/perl_mcf -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;gobmk" -o ~/dynamic/perl_gobmk -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;hmmer" -o ~/dynamic/perl_hmmer -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;sjeng" -o ~/dynamic/perl_sjeng -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;libquantum" -o ~/dynamic/perl_libq -s --smt -v fast -a ALPHA_DYN
