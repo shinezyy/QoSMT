33ab1191a406d0ad67d9fa07cb392997e20a9fef
Zhou Yaoyang <diamondzyy@sina.com>
2017-02-17 13:06:49 +0800

    remove gcc and gobmk: because perl~gcc, gobmk~mcf

--- smt_run/perl.sh
+++ smt_run/perl.sh
@@ -1,8 +1,8 @@
 ../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;perlbench" -o ~/dyn_2tcm/perl_perl -s --smt -v fast -a ALPHA_DYN
 ../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;bzip2" -o ~/dyn_2tcm/perl_bzip2 -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;gcc" -o ~/dyn_2tcm/perl_gcc -s --smt -v fast -a ALPHA_DYN
+#../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;gcc" -o ~/dyn_2tcm/perl_gcc -s --smt -v fast -a ALPHA_DYN
 ../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;mcf" -o ~/dyn_2tcm/perl_mcf -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;gobmk" -o ~/dyn_2tcm/perl_gobmk -s --smt -v fast -a ALPHA_DYN
+#../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;gobmk" -o ~/dyn_2tcm/perl_gobmk -s --smt -v fast -a ALPHA_DYN
 ../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;hmmer" -o ~/dyn_2tcm/perl_hmmer -s --smt -v fast -a ALPHA_DYN
 ../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;sjeng" -o ~/dyn_2tcm/perl_sjeng -s --smt -v fast -a ALPHA_DYN
 ../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;libquantum" -o ~/dyn_2tcm/perl_libq -s --smt -v fast -a ALPHA_DYN
