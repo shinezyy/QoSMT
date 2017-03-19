a2efbb48ca4bc70c200448b907a2118cc8e382fb
Zhou Yaoyang <diamondzyy@sina.com>
2017-03-19 11:08:14 +0800

    new selected benchmarks

--- smt_run/select.sh
+++ smt_run/select.sh
@@ -1,6 +1,7 @@
-../run_gem5_alpha_spec06_benchmark.sh -b "hmmer\;perlbench" -o ~/dyn_0315/hmmer_perl -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "hmmer\;mcf" -o ~/dyn_0315/hmmer_mcf -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "hmmer\;sjeng" -o ~/dyn_0315/hmmer_sjeng -s --smt -v fast -a ALPHA_DYN
-# ../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;mcf" -o ~/dyn_0315/perl_mcf -s --smt -v fast -a ALPHA_DYN
-# ../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;bzip2" -o ~/dyn_0315/bzip2_bzip2 -s --smt -v fast -a ALPHA_DYN
-# ../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;libquantum" -o ~/dyn_0315/bzip2_libq -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;mcf" -o ~/dyn_0317/perl_mcf -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;bzip2" -o ~/dyn_0317/bzip2_bzip2 -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;gcc" -o ~/dyn_0317/bzip2_gcc -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;libquantum" -o ~/dyn_0317/bzip2_libq -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;gcc" -o ~/dyn_0317/gcc_gcc -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;perlbench" -o ~/dyn_0317/gcc_perl -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;gobmk" -o ~/dyn_0317/gcc_gobmk -s --smt -v fast -a ALPHA_DYN
