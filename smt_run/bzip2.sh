b2d3a0e0bdbc7eb7bfead6446cff68c5b4302c07
Zhou Yaoyang <diamondzyy@sina.com>
2017-03-20 15:43:15 +0800

    change dir

--- smt_run/bzip2.sh
+++ smt_run/bzip2.sh
@@ -1,8 +1,8 @@
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;perlbench" -o ~/dyn_2tcm/bzip2_perl -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;bzip2" -o ~/dyn_2tcm/bzip2_bzip2 -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;gcc" -o ~/dyn_2tcm/bzip2_gcc -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;mcf" -o ~/dyn_2tcm/bzip2_mcf -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;gobmk" -o ~/dyn_2tcm/bzip2_gobmk -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;hmmer" -o ~/dyn_2tcm/bzip2_hmmer -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;sjeng" -o ~/dyn_2tcm/bzip2_sjeng -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;libquantum" -o ~/dyn_2tcm/bzip2_libq -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;perlbench" -o ~/dyn_0320/bzip2_perl -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;bzip2" -o ~/dyn_0320/bzip2_bzip2 -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;gcc" -o ~/dyn_0320/bzip2_gcc -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;mcf" -o ~/dyn_0320/bzip2_mcf -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;gobmk" -o ~/dyn_0320/bzip2_gobmk -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;hmmer" -o ~/dyn_0320/bzip2_hmmer -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;sjeng" -o ~/dyn_0320/bzip2_sjeng -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;libquantum" -o ~/dyn_0320/bzip2_libq -s --smt -v fast -a ALPHA_DYN
--- smt_run/select.sh
+++ smt_run/select.sh
@@ -1,7 +1,7 @@
-../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;mcf" -o ~/dyn_0317/perl_mcf -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;bzip2" -o ~/dyn_0317/bzip2_bzip2 -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;gcc" -o ~/dyn_0317/bzip2_gcc -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;libquantum" -o ~/dyn_0317/bzip2_libq -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;gcc" -o ~/dyn_0317/gcc_gcc -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;perlbench" -o ~/dyn_0317/gcc_perl -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;gobmk" -o ~/dyn_0317/gcc_gobmk -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;mcf" -o ~/dyn_0320/perl_mcf -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;bzip2" -o ~/dyn_0320/bzip2_bzip2 -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;gcc" -o ~/dyn_0320/bzip2_gcc -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;libquantum" -o ~/dyn_0320/bzip2_libq -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;gcc" -o ~/dyn_0320/gcc_gcc -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;perlbench" -o ~/dyn_0320/gcc_perl -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;gobmk" -o ~/dyn_0320/gcc_gobmk -s --smt -v fast -a ALPHA_DYN
