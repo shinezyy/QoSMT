3390ec8eb207de281efaa2350845da7a064855ea
Zhou Yaoyang <diamondzyy@sina.com>
2017-03-21 09:27:40 +0800

    add more benchmark pair scripts

---
+++ smt_run/gcc.sh
@@ -0,0 +1,8 @@
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;perlbench" -o ~/dyn_0320/gcc_perl -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;bzip2" -o ~/dyn_0320/gcc_bzip2 -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;gcc" -o ~/dyn_0320/gcc_gcc -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;mcf" -o ~/dyn_0320/gcc_mcf -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;gobmk" -o ~/dyn_0320/gcc_gobmk -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;hmmer" -o ~/dyn_0320/gcc_hmmer -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;sjeng" -o ~/dyn_0320/gcc_sjeng -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;libquantum" -o ~/dyn_0320/gcc_libq -s --smt -v fast -a ALPHA_DYN
