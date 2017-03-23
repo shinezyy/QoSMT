3585675912c5c0596b85b01112c3f0cb48ffad5c
Zhou Yaoyang <diamondzyy@sina.com>
2017-03-23 16:11:25 +0800

    add benchmark pairs running against itself

---
+++ smt_run/self.sh
@@ -0,0 +1,8 @@
+../run_gem5_alpha_spec06_benchmark.sh -b "perlbench\;perlbench" -o ~/dyn_0321/perl_perl -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "bzip2\;bzip2" -o ~/dyn_0321/bzip2_bzip2 -s --smt -v fast -a ALPHA_DYN
+#../run_gem5_alpha_spec06_benchmark.sh -b "gcc\;gcc" -o ~/dyn_0321/gcc_gcc -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "mcf\;mcf" -o ~/dyn_0321/mcf_mcf -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "gobmk\;gobmk" -o ~/dyn_0321/gobmk_gobmk -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "hmmer\;hmmer" -o ~/dyn_0321/hmmer_hmmer -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;sjeng" -o ~/dyn_0321/sjeng_sjeng -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "libquantum\;libquantum" -o ~/dyn_0321/libq_libq -s --smt -v fast -a ALPHA_DYN
