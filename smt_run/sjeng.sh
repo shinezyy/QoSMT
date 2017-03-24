8288a25dc0dd5ebf027508ec6885f509f0787d1e
Zhou Yaoyang <diamondzyy@sina.com>
2017-03-24 15:33:07 +0800

    change output dir

--- smt_run/sjeng.sh
+++ smt_run/sjeng.sh
@@ -1,8 +1,8 @@
-../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;perlbench" -o ~/dyn_0320/sjeng_perl -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;bzip2" -o ~/dyn_0320/sjeng_bzip2 -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;gcc" -o ~/dyn_0320/sjeng_gcc -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;mcf" -o ~/dyn_0320/sjeng_mcf -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;gobmk" -o ~/dyn_0320/sjeng_gobmk -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;hmmer" -o ~/dyn_0320/sjeng_hmmer -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;sjeng" -o ~/dyn_0320/sjeng_sjeng -s --smt -v fast -a ALPHA_DYN
-../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;libquantum" -o ~/dyn_0320/sjeng_libq -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;perlbench" -o ~/dyn_bpp2/sjeng_perl -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;bzip2" -o ~/dyn_bpp2/sjeng_bzip2 -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;gcc" -o ~/dyn_bpp2/sjeng_gcc -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;mcf" -o ~/dyn_bpp2/sjeng_mcf -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;gobmk" -o ~/dyn_bpp2/sjeng_gobmk -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;hmmer" -o ~/dyn_bpp2/sjeng_hmmer -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;sjeng" -o ~/dyn_bpp2/sjeng_sjeng -s --smt -v fast -a ALPHA_DYN
+../run_gem5_alpha_spec06_benchmark.sh -b "sjeng\;libquantum" -o ~/dyn_bpp2/sjeng_libq -s --smt -v fast -a ALPHA_DYN
