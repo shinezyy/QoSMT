a3c0d4a718078f34cec29b7dc0ec9f5e80ecd47d
Zhou Yaoyang <diamondzyy@sina.com>
2017-04-26 15:00:52 +0800

    check timestamp between gem binary and output_dir

--- util/run_smt_cpt.py
+++ util/run_smt_cpt.py
@@ -15,6 +15,9 @@ from multiprocessing import Pool
 from common import *
 
 
+output_dir = '~/dyn_0425'
+
+
 def get_pairs():
     x = []
     with open('./selected_pairs') as f:
@@ -36,11 +39,32 @@ def cpt_filter(pairs):
     return ret
 
 
+def time_stamp_filter(pairs):
+    global output_dir
+    gem5_m_time = os.path.getmtime(pjoin(os.environ['gem5_build'], 'gem5.fast'))
+    ret = []
+
+    for pair in pairs:
+        pair_dir = pair[0] + '_' + pair[1]
+        benchmark_dir = pjoin(uexp(output_dir), pair_dir)
+        time_stamp_file = pjoin(uexp(benchmark_dir), 'done')
+        if os.path.isdir(benchmark_dir) and os.path.isfile(time_stamp_file):
+            file_m_time = os.path.getmtime(time_stamp_file)
+            if file_m_time >= gem5_m_time:
+                print 'Time stamp of {} in {} is ' \
+                        'newer than gem5 binary, skip!'.format(pair, output_dir)
+                continue
+        ret.append(pair)
+    return ret
+
+
 def smt_run(pair):
     gem5_dir = os.environ['gem5_root']
     pair_dir = pair[0] + '_' + pair[1]
     merged_cpt_dir_ = pjoin(merged_cpt_dir(), pair_dir)
-    outdir = pjoin(uexp('~/dyn_0425'), pair_dir)
+    global output_dir
+    outdir = pjoin(uexp(output_dir), pair_dir)
+
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     exec_dir = pjoin(gem5_dir, 'smt_run')
@@ -81,12 +105,16 @@ def smt_run(pair):
         *options
     )
 
+    sh.touch(pjoin(outdir, 'done'))
+
 
 if __name__ == '__main__':
-    global num_thread
     num_thread  = 10
-    all_targets = get_pairs()
-    targets = cpt_filter(all_targets)
+
+    targets = get_pairs()
+    targets = cpt_filter(targets)
+    targets = time_stamp_filter(targets)
+
 
     print 'Following {} pairs will be run'.format(len(targets))
     print_list(targets)
