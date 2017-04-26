15023274bf83558221a3aeddc9eb587ec2d74dcc
Zhou Yaoyang <diamondzyy@sina.com>
2017-04-26 09:36:49 +0800

    backup st simpoints

--- util/simpoint_bak.py
+++ util/simpoint_bak.py
@@ -20,7 +20,8 @@ num_thread = 18
 def get_spec():
     x = []
     print 'This script should not be used again, check please'
-    with open('./simpointed_spec.txt') as f:
+    assert(0)
+    with open('./checkpointed.txt') as f:
         for line in f:
             if not line.startswith('#'):
                 x.append(line.strip('\n'))
@@ -29,23 +30,23 @@ def get_spec():
 
 def simpoint_bak(benchmark):
     gem5_dir = os.environ['gem5_root']
-    all_checkpoing_dir = pjoin(gem5_dir, 'checkpoint')
+    all_checkpoint_dir = pjoin(gem5_dir, 'checkpoint')
     all_simpoint_dir = pjoin(gem5_dir, 'simpoint')
 
-    benchmark_dir = pjoin(all_checkpoing_dir, benchmark)
+    benchmark_dir = pjoin(all_checkpoint_dir, benchmark)
     simpoint_dir = pjoin(all_simpoint_dir, benchmark)
 
-    backup_dir = pjoin(uexp('~/simpoint_bak'), benchmark)
+    backup_dir = pjoin(uexp('~/checkpoint_bak'), benchmark)
 
     print 'from {} to {}'.format(simpoint_dir, backup_dir)
 
-    sh.cp('-r', simpoint_dir, backup_dir)
+    sh.cp('-r', benchmark_dir, backup_dir)
 
 
 
 if __name__ == '__main__':
     global num_thread
-    num_thread = 6
+    num_thread = 18
     bms = get_spec()
     print len(bms), 'benchmarks:', bms, 'will be backup'
     user_verify()
