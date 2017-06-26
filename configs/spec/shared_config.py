4b933e64cd67337723ef268ea79c77c2e16327f4
Zhou Yaoyang <diamondzyy@sina.com>
2017-06-26 16:01:19 +0800

    change max insts for debug

--- configs/spec/shared_config.py
+++ configs/spec/shared_config.py
@@ -1,6 +1,6 @@
-dumpWindowSize = 5*(10**6)
+dumpWindowSize = 4*(10**6)
 policyWindowSize = (10**3)*20
 LQEntries = 48
 SQEntries = 40
-max_insts_hpt_thread = 75*(10**6)
+max_insts_hpt_thread = 9*(10**6)
 
